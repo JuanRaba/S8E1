805de2f4721f0374a13a589655b1fc140b710478
juanraba <jrabanillog@gmail.com>
2018-07-13 11:53:11 -0400

    fix ejer4.4

--- ejer4.rb
+++ ejer4.rb
@@ -3,22 +3,27 @@ option = 0
 
 def product_sum(array)
   sum = array.last(array.size - 1).inject (0) { |mem, var| mem + var.to_i }
-  "#{e.first} #{sum}"
+  [array.first, sum]
 end
 
-def when1
+def read_data
   data = []
   File.open('ejer4.data', 'r') do |file|
     data = file.readlines.map do |line|
-      line.split(',').map { |e| e.chomp.gsub(/[NR]/, '0') }
+      line.split(', ').map { |e| e.chomp.gsub(/[NR]/, '0') }
     end
   end
+  data
+end
 
+def when1
+  data = read_data()
   puts 'permite conocer la cantidad de productos existentes'
-  puts 'a) Mostrar la existencia por productos.
-b) Mostrar la existencia total por tienda.
-c) Mostrar la existencia total en todas las tiendas.
-d) Volver al menú principal'
+  puts '
+  a) Mostrar la existencia por productos.
+  b) Mostrar la existencia total por tienda.
+  c) Mostrar la existencia total en todas las tiendas.
+  d) Volver al menú principal'
   exit = false
   until exit
     option = gets.chomp
@@ -26,7 +31,8 @@ d) Volver al menú principal'
     case option
     when 'a'
       data.each do |e|
-        puts product_sum(e)
+        arr = product_sum(e)
+        puts "#{arr.first} #{arr.last} "
       end
     when 'b'
       # fixed i makes posible to inject by tienda
@@ -60,10 +66,34 @@ while option != 6
     prod_wanted = gets.chomp
     File.open('ejer4.data', 'r') do |file|
       file.readlines.each do |line|
-        prod = line.split(',').map { |e| e.chomp.gsub(/[NR]/, '0') }
-        puts product_sum(prod) if prod.first == prod_wanted
+        prod = line.split(', ').map { |e| e.chomp.gsub(/[NR]/, '0') }
+        puts product_sum(prod).to_s if prod.first == prod_wanted
       end
     end
+  when 3
+    puts 'ingresa nombre del producto'
+    data = read_data()
+    # fixed i makes posible to inject by tienda
+    for i in 1..(data.first.size - 1)
+      puts "No registrados en almacen #{i}"
+      data.each do |prod|
+        puts prod.first if prod[i] == '00'
+      end
+    end
+  when 4
+    puts 'ingresa valor para conocer productos con menor existencia'
+    max_wanted = gets.chomp.to_i
+    data = read_data()
+    File.open('ejer4.data', 'r') do |file|
+      puts "finding prods with stock less than #{max_wanted}"
+      file.readlines.each do |line|
+        prod = line.split(', ').map { |e| e.chomp.gsub(/[NR]/, '0') }
+        psum = product_sum(prod)
+        puts psum.to_s if (psum.last.to_i <= max_wanted)
+      end
+    end
+  when 5
+    
   else
     puts 'Por favor, ingresa num del 1 al 6!'
   end
--- test.rb
+++ test.rb
@@ -1,7 +1,7 @@
 data = []
 File.open('ejer4.data', 'r') do |file|
   data = file.readlines.map do |line|
-    line.split(',').map { |e| e.chomp.gsub(/[NR]/, '0') }
+    line.split(', ').map { |e| e.chomp.gsub(/[NR]/, '0') }
   end
 end
 puts data.to_s
@@ -30,13 +30,31 @@ prod_wanted = str
 
 File.open('ejer4.data', 'r') do |file|
   file.readlines.each do |line|
-    prod = line.split(',').map { |e| e.chomp.gsub(/[NR]/, '0') }
+    prod = line.split(', ').map { |e| e.chomp.gsub(/[NR]/, '0') }
     puts product_sum(prod) if prod.first == prod_wanted
   end
 end
 
-data.each_with_index do |_tienda, i|
-  if i != 0
-    puts "tienda#{i} #{data.inject(0) { |mem, var| mem + var[i].to_i }}"
+def read_data
+  data = []
+  File.open('ejer4.data', 'r') do |file|
+    data = file.readlines.map do |line|
+      line.split(', ').map { |e| e.chomp.gsub(/[NR]/, '0') }
+    end
+  end
+  data
+end
+data = read_data
+def product_sum(array)
+  sum = array.last(array.size - 1).inject (0) { |mem, var| mem + var.to_i }
+  [array.first, sum]
+end
+max_wanted = 5
+File.open('ejer4.data', 'r') do |file|
+  puts "finding prods with stock less than #{max_wanted}"
+  file.readlines.each do |line|
+    prod = line.split(', ').map { |e| e.chomp.gsub(/[NR]/, '0') }
+    psum = product_sum(prod)
+    puts psum.to_s if (psum.last.to_i <= max_wanted)
   end
 end
